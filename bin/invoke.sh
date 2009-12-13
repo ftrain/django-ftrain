d2fa48c58b86e180796eb27956ccce92b2d54a51
Paul Ford <ford@ftrain.com>
2009-12-13 12:37:52 -0500

    Invoke

--- bin/invoke.sh
+++ bin/invoke.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# Quick hack to run a custom manage_* script based on uname; thus I
+# can have manage_home, manage_work, etc, although it would make far
+# more sense to modify settings based on hostname and hack my own
+# settings.py file.
+
 ./manage_`uname -n`.py $1 $2 $3 $4 $5;
